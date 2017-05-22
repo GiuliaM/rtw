aae3cfc1c9bc47846f01e44d41eecf28772915cf
GiuliaM <Gekke_giulia@hotmail.com>
2017-05-22 16:56:39 +0200

    Start over with Spotify search

--- app.js
+++ app.js
@@ -1,178 +1,41 @@
-var request = require('request'); // require "Request" library
-var querystring = require('querystring'); //parsing and formatting URL query strings
-var env = require('dotenv').config(); //loads environment variables into process.env
 var express = require('express'); // Express web server framework
-var session = require('express-session') // Session to save an accesstoken
+var bodyParser = require('body-parser'); // parses the body of an http-request to JSON
+var request = require('superagent');
 
 var app = express();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
 
-server.listen(process.env.PORT || 3000);
-console.log('server listening on port 3000');
+// set the view engine to ejs
+app.set('view engine', 'ejs');
 
-app.use('/static', express.static('./static')); //app looks in the static folder
+//app looks in the static folder
+app.use('/static', express.static('./static'));
 
-var client_id = process.env.CLIENT_ID; // Your client id
-var client_secret = process.env.CLIENT_SECRET; // Your secret
-var redirect_uri = process.env.REDIRECT_URI; // Your redirect uri
-var users = [];
-var connections= [];
-
-app.use(session({
-    secret: 'just another secret',
-    saveUninitialized: true,
-    resave: false
-}));
-
-app.set('view engine', 'ejs'); // set the view engine to ejs
+//translate all x-www-form-urlencoded to JSON
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', function(req, res) {
-//    var sess = req.session;
-    res.render('index');
-})
-
-app.get('/login', function(req, res) {
-  var permissions = encodeURIComponent('client'); //It's necessary to encode the permissions string to allow for scopes like channels:read
-
-  console.log("serving Spotify button with permissions: " + permissions);
-
-  // Here we need request.app to access Express instance called 'app' we created in our main server file
-  if (req.session.accessToken){ // controle if the oauth has been done
-    console.log("already have oauth, reroute client to /playlist");
-    res.redirect('/playlist');
-  } else { // if it doesn't exist, ask permission for this information
-    console.log("new instance");
-    var scope = 'user-read-private user-read-email user-read-currently-playing user-read-playback-state user-read-recently-played';
-
-    res.redirect('https://accounts.spotify.com/authorize?' +
-      querystring.stringify({
-        response_type: 'code',
-        client_id: client_id,
-        scope: scope,
-        redirect_uri: redirect_uri
-        }));
-  }
+    res.render('index', {
+        items: []
+    });
 });
 
-app.get('/callback', function(req, res) {
-  var code = req.query.code; //code that appears in the url
+app.post('/', function(req, res) {
+    var apiUrl = 'https://api.spotify.com/v1/search?type=track&q=' + req.body.searchQuery;
 
-  var authOptions = { // Info to send the tokens to spotify
-    url: 'https://accounts.spotify.com/api/token',
-    form: {
-      code: code,
-      redirect_uri: redirect_uri,
-      grant_type: 'authorization_code'
-    },
-    headers: {
-      'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64')) //base64 = binary-to-text encoding
-    },
-    json: true
-  };
+    request
+        .get(apiUrl)
+        .end(function(err, spotifyData) {
+            if(err) {
+                console.log(err);
+                return res.send(err);
+            }
 
-  // Sending tokens to spotify to ask permission to access the API
-  request.post(authOptions, function(error, response, body) {
-    req.session.accessToken = body.access_token;
-    req.session.refreshToken = body.refresh_token;
-    res.redirect('/playlist'); // redirect to playlist.ejs
-  });
-});
-
-app.get('/playlist', function(req, res) { // render the playlist page
-    if (req.session.accessToken) {
-        var options = {
-            url: 'https://api.spotify.com/v1/me/player/currently-playing',
-            headers: {
-                Authorization: 'Bearer ' + req.session.accessToken
-            },
-            json: true
-        };
-
-        request.get(options, function(error, response, body) {
-            if (error) throw error;
-            req.session.currentlyPlaying = body.item.id;
-            res.render('playlist', {
-                artists: body.item.artists,
-                album: body.item.album,
-                name: body.item.name
+            res.render('index', {
+                items: spotifyData.body.tracks.items
             });
         });
-
-      io.on('connection', function(socket) {
-          setInterval(function(){
-              request.get(options, function(error, response, body) {
-                  console.log(req.session.currentlyPlaying, body.item.id);
-                if (req.session.currentlyPlaying !== body.item.id) {
-                    req.session.currentlyPlaying = body.item.id;
-                    socket.emit('update song', {
-                        artists: body.item.artists,
-                        album: body.item.album,
-                        name: body.item.name
-                    });
-                }
-              });
-          }, 1000);
-
-          // When a client disconnects, do
-          socket.on('disconnect', function(data){
-              // remove the user from the users array
-              users.splice(users.indexOf(socket.username), 1);
-          });
-      });
-  } else {
-    res.redirect('/');
-  }
 });
 
-app.get('/chatroom', function(req, res) { // render the chatroom page
-
-    var profile = {
-      url: 'https://api.spotify.com/v1/me',
-      headers: { 'Authorization': 'Bearer ' + req.session.accessToken },
-      json: true
-    }
-
-    request.get(profile, function(error, response, body) {
-      var nickname = "";
-      var nickname = body.display_name;
-
-//      console.log(body);
-
-          if(body.display_name === null){
-            var nickname = body.id;
-          } else {
-            var nickname = body.display_name;
-          }
-      console.log(body.id, "DIT IS HET ID");
-      console.log(body.display_name, "DIT IS DE DISPLAY NAME");
-
-      res.render('chatroom', {body: body});
-
-    io.on('connection', function(socket){
-      connections.push(socket);
-      console.log('Connected: %s sockets connected', connections.length);
-
-      socket.on('send message', function(data){
-          io.sockets.emit('new message', {msg: data, user: nickname});
-    });
-
-    socket.on('new user', function(data, callback){
-      callback(true);
-      nickname = data;
-      users.push(nickname);
-      updateUsernames();
-    });
-
-      // Disconnect
-    socket.on('disconnect', function(data){
-        users.splice(users.indexOf(socket.username), 1);
-        console.log('Disconnected: %s sockets connected', connections.length);
-    });
-
-    function updateUsernames(){
-      io.sockets.emit('get users', users);
-    }
-});
-    });
+app.listen(3000, function() {
+    console.log('server listening on port 3000');
 });
